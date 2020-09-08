d49bf8133eb3e3dfeec5e7153f22c1b778fbb51c
B4TT3RY <gb4tt3ry@gmail.com>
2020-09-08 22:04:31 +0900

    Remove Unusual Code and Print

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,8 +1,7 @@
 pub async fn send_message(bot_token: &str, chat_id: &str, message: &str) {
     let url = format!("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=MarkdownV2&disable_web_page_preview=true", bot_token, chat_id, message);
-    let url: reqwest::Url = url.parse().unwrap();
-    let res = reqwest::get(url).await.unwrap();
-    println!("{}\n{}", res.status(), res.text().await.unwrap());
+    let res = reqwest::get(&url).await.unwrap();
+    println!("{}", res.status());
 }
 
 pub fn syntax(message: &str) -> String {
